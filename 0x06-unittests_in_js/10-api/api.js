b81dfc6b973ee766a294eb54119ac20afa65ea3f
BezaB10 <beza_b@outlook.com>
2023-03-06 20:24:17 +0300

    edited

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -19,3 +19,15 @@ app.get('/available_payments', (req, res) => {
       credit_cards: true,
       paypal: false,
     },
+  };
+  res.json(obj);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.userName;
+  res.end(`Welcome ${username}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+i});
