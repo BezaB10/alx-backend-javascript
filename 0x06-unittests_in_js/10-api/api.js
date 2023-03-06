dd17deb2f0a9704dd6e315f2ad785ee3ebb83524
BezaB10 <beza_b@outlook.com>
2023-03-06 20:49:06 +0300

    edited

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,31 +3,19 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.use(express.json());
-
 app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
-});
-
-app.get('/available_payments', (req, res) => {
-  const obj = {
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
-    },
-  };
-  res.json(obj);
-});
-
-app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.end(`Welcome ${username}`);
-});
+app.get('/cart/:id', (req, res) => {
+  if (typeof(req.params.id) == 'number') {
+  res.status(200).send(`Payment methods for cart ${req.params.id}`);
+  }
+  res.status(404);
+})
 
 app.listen(port, () => {
-  console.log('API available on localhost port 7865');
-i});
+    console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
