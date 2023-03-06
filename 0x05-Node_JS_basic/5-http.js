90a3ec1f1ddbf9b39dc8e7da290731fff735998a
BezaB10 <beza_b@outlook.com>
2023-03-06 21:30:11 +0300

    edited

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,20 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const getStudentData = require('./3-read_file_async');
-
-const PORT = 1245;
-const HOST_NAME = '127.0.0.1';
-
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  }
+  if (req.url === '/') res.write('Hello Holberton School!');
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    getStudentData(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}`);
-      res.end();
-    }).catch((err) => res.end(err.message));
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.end();
 });
-
-app.listen(PORT, HOST_NAME, () => {
-  console.log(`Server running at http://${HOST_NAME}:${PORT}`);
-});
-
+app.listen(1245);
 module.exports = app;
