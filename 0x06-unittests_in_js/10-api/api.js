6245c95f6a3d17a74d9b061c3a606349259bcfb3
BezaB10 <beza_b@outlook.com>
2023-03-06 20:57:26 +0300

    edited

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,21 +1,33 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const port = 7865;
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  if (typeof(req.params.id) == 'number') {
-  res.status(200).send(`Payment methods for cart ${req.params.id}`);
-  }
-  res.status(404);
-})
+app.get('/cart/:id([0-9]+)', function (req, res) {
+  res.send(`Payment methods for cart ${req.params.id}`);
 
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
 });
 
-module.exports = app;
+app.get('/available_payments', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  res.send(`Welcome ${req.body.userName}`);
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
